2ff19e4eb598d9f06c4bded0ce642eb1437c06c7
Alessandro De Blasis <alex@deblasis.net>
2021-11-19 01:36:34 +0000

    fix
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- Transcribe/index.js
+++ Transcribe/index.js
@@ -1,90 +1,81 @@
-
-const fs = require('fs');
-const tmp = require('tmp');
-const axios = require('axios');
-const stream = require('stream');
-const { promisify } = require('util');
+const fs = require("fs");
+const tmp = require("tmp");
+const axios = require("axios");
+const stream = require("stream");
+const { promisify } = require("util");
 const crypto = require("crypto");
 
-
-const {uploadBlob} = require('../common/storage');
-
+const { uploadBlob, updateJobProgress } = require("../common/storage");
 
 const code = process.env.AuthCode;
 
-
-
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const authCode = req.headers["authorization"];
-    if (!authCode || authCode != code) {
-        context.res = {
-            status: 403,
-            body: "Unauthorized"
-        }
-        context.done();
-    }
-
-
-    const { fileUrl, userId } = req.body;
-    context.log(`req: fileUrl ${fileUrl} userId: ${userId}`);
-
-    const finished = promisify(stream.finished);
-
-    const tmpFile = tmp.fileSync();
-
-    const file = fs.createWriteStream(tmpFile.name);
-
-
-    context.log(`file downloading from ${fileUrl}`);
-    await axios({
-        method: "get",
-        url: fileUrl,
-        responseType: "stream"
-    }).then(function (response) {
-        response.data.pipe(file);
-        context.log(`file downloaded as ${tmpFile.name}`);
-        return finished(file);
-    });
-
-    const hash = hashFile(tmpFile.name);
-    context.log(`hash ${hash}`);
-
-    const job = {
-        fileName: fileUrl,
-        rawFile: `raw/${userId}-_-${hash}`,
-        cleanAudioFile: "",
-        transcription: ""
-    }
-    try {
-        await uploadBlob("raw", `${userId}-_-${hash}`, tmpFile.name);
-        context.log(`file stored in blobstorage as raw/${userId}-_-${hash}`);
-    } catch (err) {
-        context.log(`error`,err);
-    }
-
+  const authCode = req.headers["authorization"];
+  if (!authCode || authCode != code) {
     context.res = {
-        body: JSON.stringify(job)
-    }
-
-    await updateJobProgress(userId, hash, {
-        ...job
-    });
-
+      status: 403,
+      body: "Unauthorized",
+    };
     context.done();
-    try {
-        tmpFile.removeCallback();
-    } catch (err) {
-        context.log(`error`,err);
-    }
-}
+  }
 
+  const { fileUrl, userId } = req.body;
+  context.log(`req: fileUrl ${fileUrl} userId: ${userId}`);
 
+  const finished = promisify(stream.finished);
+
+  const tmpFile = tmp.fileSync();
+
+  const file = fs.createWriteStream(tmpFile.name);
+
+  context.log(`file downloading from ${fileUrl}`);
+  await axios({
+    method: "get",
+    url: fileUrl,
+    responseType: "stream",
+  }).then(function (response) {
+    response.data.pipe(file);
+    context.log(`file downloaded as ${tmpFile.name}`);
+    return finished(file);
+  });
+
+  const hash = hashFile(tmpFile.name);
+  context.log(`hash ${hash}`);
+
+  const job = {
+    fileName: fileUrl,
+    rawFile: `raw/${userId}-_-${hash}`,
+    cleanAudioFile: "",
+    transcription: "",
+  };
+  try {
+    await uploadBlob("raw", `${userId}-_-${hash}`, tmpFile.name);
+    context.log(`file stored in blobstorage as raw/${userId}-_-${hash}`);
+  } catch (err) {
+    context.log(`error`, err);
+  }
+
+  context.res = {
+    body: JSON.stringify(job),
+  };
+
+  await updateJobProgress(userId, hash, {
+    ...job,
+  });
+
+  context.done();
+  try {
+    tmpFile.removeCallback();
+  } catch (err) {
+    context.log(`error`, err);
+  }
+};
 
 function hashFile(fileName) {
-    const fileBuffer = fs.readFileSync(fileName);
-    const hashSum = crypto.createHash('sha1');
-    hashSum.update(fileBuffer);
-    return hashSum.digest('hex');
-  }
+  const fileBuffer = fs.readFileSync(fileName);
+  const hashSum = crypto.createHash("sha1");
+  hashSum.update(fileBuffer);
+  return hashSum.digest("hex");
+}
