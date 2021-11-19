72f29b8cc5e56d56128cf3bc256ea112305d45bf
Alessandro De Blasis <alex@deblasis.net>
2021-11-19 01:26:07 +0000

    fix storage
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- common/storage.js
+++ common/storage.js
@@ -1,37 +1,30 @@
-const {
-  TableClient,
-  AzureNamedKeyCredential,
-} = require("@azure/data-tables");
-const { BlobServiceClient, StorageSharedKeyCredential  } = require('@azure/storage-blob');
+const { TableClient } = require("@azure/data-tables");
+const { BlobServiceClient } = require("@azure/storage-blob");
 
-const account = process.env.StorageAccountName;
-const accountKey = process.env.StorageKey;
-
+const connStr = process.env.StorageConnectionString;
 const jobsTableName = "Jobs";
-const storageAddress = (type) => `https://${account}.${type}.core.windows.net`;
 
-const ankCred = new AzureNamedKeyCredential(account, accountKey);
-const sskCred = new StorageSharedKeyCredential(account, accountKey);
-
-const blobServiceClient = new BlobServiceClient(storageAddress('blob'), ankCred);
-const jobsTableClient = new TableClient(storageAddress('table'), jobsTableName, sskCred);
-
+const jobsTableClient = TableClient.fromConnectionString(
+  connStr,
+  jobsTableName
+);
+const blobServiceClient = BlobServiceClient.fromConnectionString(connStr);
 
 module.exports = {
-  uploadBlob: async function(bucket, filename, file) {
-        const containerClient = blobServiceClient.getContainerClient(bucket);
-        const blockBlobClient = containerClient.getBlockBlobClient(filename);
-        return await blockBlobClient.uploadFile(file);
-    },
-    updateJobProgress: async function (userId, fileHash, props) {
-        const entity = {
-            partitionKey: userId,
-            rowKey: fileHash,
-            ...props
-            };
-            return await jobsTableClient.upsertEntity(entity);
-    },
-    getJobProgress: async function (userId, fileHash) {
-        return await jobsTableClient.getEntity(userId, fileHash);
-    }
-  }
+  uploadBlob: async function (bucket, filename, file) {
+    const containerClient = blobServiceClient.getContainerClient(bucket);
+    const blockBlobClient = containerClient.getBlockBlobClient(filename);
+    return await blockBlobClient.uploadFile(file);
+  },
+  updateJobProgress: async function (userId, fileHash, props) {
+    const entity = {
+      partitionKey: userId,
+      rowKey: fileHash,
+      ...props,
+    };
+    return await jobsTableClient.upsertEntity(entity);
+  },
+  getJobProgress: async function (userId, fileHash) {
+    return await jobsTableClient.getEntity(userId, fileHash);
+  },
+};
